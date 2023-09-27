e3b467a03003863fe8a9fdf3ef785bcb361695e3
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-27 22:23:09 +0530

    add two element using <></>

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,7 @@
-import { Fragment } from "react";
 
 function ListGroup() {
   return (
-  <Fragment>
+  <>
     <h1>List</h1>
     <ul className="list-group">
       <li className="list-group-item">An item</li>
@@ -11,7 +10,7 @@ function ListGroup() {
       <li className="list-group-item">A fourth item</li>
       <li className="list-group-item">And a fifth one</li>
     </ul>
-  </Fragment>
+  </>
 
 
 
