89e38e011aeb54a6808a2dfc373224b6c9bcd8a7
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-28 09:43:13 +0530

    print item and index

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -11,8 +11,13 @@ function ListGroup() {
     <h1>List</h1>
     {items.length === 0 && <p>No item found</p>}
     <ul className="list-group">
-      {items.map((item)=> (
-        <li className="list-group-item" key={item} onClick={() => console.log('Clicked')}>{item}</li>
+      {items.map((item,index)=> (
+        <li 
+        className="list-group-item" 
+        key={item} 
+        onClick={() => console.log(item,index)} >
+            {item}
+        </li>
       ))}
     </ul>
   </>
