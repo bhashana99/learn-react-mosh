0393bcfed1bea3204fdc9c61c157eabe83491968
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-28 09:35:48 +0530

    add bootstrap class to list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,7 +3,7 @@ function ListGroup() {
 
   let items = ["New York", "San Francisco", "Tokyo", "London","Paris"];
 
-  items = [];  //re assign
+  
 
 
   return (
@@ -12,7 +12,7 @@ function ListGroup() {
     {items.length === 0 && <p>No item found</p>}
     <ul className="list-group">
       {items.map((item)=> (
-        <li key={item}>{item}</li>
+        <li className="list-group-item" key={item}>{item}</li>
       ))}
     </ul>
   </>
