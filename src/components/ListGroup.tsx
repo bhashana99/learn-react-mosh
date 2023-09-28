ba41c0a61dc2332b7ddc14fcf7bfecbe8c527803
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-28 09:38:27 +0530

    add  event listener

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,7 +12,7 @@ function ListGroup() {
     {items.length === 0 && <p>No item found</p>}
     <ul className="list-group">
       {items.map((item)=> (
-        <li className="list-group-item" key={item}>{item}</li>
+        <li className="list-group-item" key={item} onClick={() => console.log('Clicked')}>{item}</li>
       ))}
     </ul>
   </>
