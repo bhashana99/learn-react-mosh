d915c8d50dd260c9b02f60f4d219a6eea1545565
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-28 09:25:36 +0530

    inside return include condition

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,12 +5,10 @@ function ListGroup() {
 
   items = [];  //re assign
 
-  if(items.length === 0)
-  return <><h1>List</h1><p>No item found</p></>
-
   return (
   <>
     <h1>List</h1>
+    {items.length === 0 ? <p>No item found</p> : null}
     <ul className="list-group">
       {items.map((item)=> (
         <li key={item}>{item}</li>
